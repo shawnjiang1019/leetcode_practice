a4fccbe709caac51ad12eac78a474a86f33ef2f3
Shawn <64927869+shawnjiang1019@users.noreply.github.com>
2026-02-17 12:34:25 -0500

    Time: 27 ms (31.82%) | Memory: 19.8 MB (23.15%) - LeetSync

---
+++ 1176-design-a-leaderboard/design-a-leaderboard.py
@@ -0,0 +1,23 @@
+class Leaderboard:
+
+    def __init__(self):
+        self.heap = []
+        self.scores = {}
+
+    def addScore(self, playerId: int, score: int) -> None:
+        self.scores[playerId] = self.scores.get(playerId, 0) + score
+            
+
+    def top(self, K: int) -> int:
+        return sum(heapq.nlargest(K, self.scores.values()))
+
+    def reset(self, playerId: int) -> None:
+        self.scores[playerId] = 0
+
+
+
+# Your Leaderboard object will be instantiated and called as such:
+# obj = Leaderboard()
+# obj.addScore(playerId,score)
+# param_2 = obj.top(K)
+# obj.reset(playerId)
